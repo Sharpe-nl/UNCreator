25f43fcbcad930969b96affca3742dc79594498f
Sh4rpe <sharpe-1984@outlook.com>
2020-02-18 13:29:00 +0100

    Added inputparser, needs testing

--- unc.py
+++ unc.py
@@ -1,22 +1,24 @@
-infile = "testfile.txt"
-outfile = "out.txt"
-# https://www.owasp.org/index.php?title=Testing_for_Default_or_Guessable_User_Account_(OWASP-AT-003)&setlang=es
-defaultList = ["admin", "administrator", "root", "system", "guest", "operator", "super"]
+import optparse
+import time
 
 class UNC:
 
-    def __init__(self, infile, outfile):
+    def __init__(self, infile, outfile, numberadd, casesense):
         self.infile = infile
         self.outfile = outfile
+        self.numberadd = numberadd
+        self.casesense = casesense
+        # https://www.owasp.org/index.php?title=Testing_for_Default_or_Guessable_User_Account_(OWASP-AT-003)&setlang=es
+        self.defaultList = ["admin", "administrator", "root", "system", "guest", "operator", "super"]
 
-    def createOptions(self, user, mrange=-1, caseSensitive=False):
+    def createOptions(self, user):
         firstname, secondname = user.strip().split(" ")
-        if caseSensitive:
+        if self.casesense:
             """If case sensitive is set, repeat the process for also only non capitalized input"""
-            self.createOptions(user.lower(), mrange)
+            self.createOptions(user.lower(), self.numberadd)
         options = self.applyOptionRules(firstname, secondname)
-        if mrange >= 0:
-            options = options + self.addNumbersToOptions(options, mrange)
+        if self.numberadd >= 0:
+            options = options + self.addNumbersToOptions(options, self.numberadd)
         with open(self.outfile, 'a') as f:
             for username in options:
                 f.write(username + "\n")
@@ -51,9 +53,38 @@ class UNC:
                 if user == "\n":
                     pass
                 else:
-                    self.createOptions(user, 2)
+                    self.createOptions(user)
 
 
 if __name__ == "__main__":
-    unc = UNC(infile, outfile)
+    main()
+
+def main():
+    infile = "testfile.txt"
+    outfile = "out.txt"
+
+    parser = optparse.OptionParser(usage='usage: '+__file__+ ' -i inputfile ')
+    parser.add_option('-i', '--inputfile', dest='infile', type='string', help='Specify a file containing a list of first and second names (e.g. John Doe')
+    parser.add_option('-o', '--outputfile', dest='outfile', type='string', help='Specify an outputfile, otherwise a custom name will be created')
+    parser.add_option('-n', '--numberadd', dest='numberadd', type='string', help='Additionally create usernames with appended number, default is no number')
+    parser.add_option('-c', '--casesensitive', dest='casesense', default=False, help='State \"True\" if you want case sensitivity activated, this will result in a list that is two times the size, default=False')
+
+    (options, args) = parser.parse_args()
+    if options.infile == None:
+        print('[-] No inputfile specified')
+        print(parser.usage())
+        exit(0)
+    if options.outputfile == None:
+        outfile = "usernames_" + time.strftime("%Y%m%d-%H%M%S")
+        print('[-] No outputfile specified, creating default file {}'.format(outfile))
+    else:
+        outfile = options.outfile
+    if options.numberadd == None:
+        numberadd = -1
+    else:
+        numberadd = options.numberadd
+
+    print('[+] Starting creating username file')
+    unc = UNC(options.infile, outfile, numberadd, options.casesense)
     unc.run()
+    print('[+] List creation finished: {}'.format(outfile))
